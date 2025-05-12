6aa8d2866aa9ce76b87e0be7e9783a879211d51b
SHOURYA DHATRIKA <shourya.dhatrika7@gmail.com>
2025-05-12 22:54:44 +0530

    Create Subsets of an Array.py

---
+++ Solutions/Subsets of an Array.py
@@ -0,0 +1,57 @@
+'''
+Subsets of an Array
+Given an array of unique integer elements, print all the subsets of the given array in lexicographical order.
+
+Input Format
+The first line of input contains T - the number of test cases. It's followed by 2T lines, the first line contains N - the size of the array and the second line contains the elements of the array.
+
+Output Format
+For each test case, print the subsets of the given array in lexicographical order, separated by new line. Print an extra newline between output of different test cases.
+
+Constraints
+1 <= T <= 100
+1 <= N <= 10
+0 <= A[i] <= 100
+
+Example
+Input
+3
+3
+5 15 3 
+2
+57 96 
+4
+3 15 8 23 
+
+Output
+3 
+3 5 
+3 5 15 
+3 15 
+5 
+5 15 
+15 
+
+57 
+57 96 
+96 
+
+3 
+3 8 
+3 8 15 
+3 8 15 23 
+3 8 23 
+3 15 
+3 15 23 
+3 23 
+8 
+8 15 
+8 15 23 
+8 23 
+15 
+15 23 
+23
+
+Explanation
+
+Self Explanatory'''
